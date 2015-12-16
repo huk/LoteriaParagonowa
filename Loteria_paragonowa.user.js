a5550be96d7adba18a20d10735814191cd21161f
Piotr Huk <piotr@huk.waw.pl>
2015-12-16 20:26:15 +0100

    Userscript

---
+++ Loteria_paragonowa.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Loteria paragonowa
+// @namespace   huk.waw.pl
+// @include     https://loteriaparagonowa.gov.pl/
+// @version     1
+// @grant       none
+// ==/UserScript==
+function pad(n){return n<10 ? '0'+n : n}
+var operation = $("#captcha-operation").html();
+var result = eval(operation);
+var now = new Date();
+$("#captcha-input").val(result);
+$("#zgoda_dane").prop("checked", "checked");
+$("#sprawdzone").prop("checked", "checked");
+$("#rok").val(now.getFullYear());
+$("#miesiac").val(pad(now.getMonth()+1));
