532587328cb5bd7c2fe2b31b73cd206e77a1a9a1
huk <piotr@huk.waw.pl>
2016-06-12 17:25:57 +0200

    Clear all fields

--- Loteria_paragonowa.user.js
+++ Loteria_paragonowa.user.js
@@ -7,6 +7,11 @@
 // ==/UserScript==
 function pad(n){return n<10 ? '0'+n : n}
 $(function(){
+	$("#nr_kasy_1").val("");
+	$("#nip_1,#nip_2,#nip_3,#nip_4,#nip_5").val("");
+	$("#nip_6,#nip_7,#nip_8,#nip_9,#nip_10").val("");
+	$("#rok,#miesiac,#dzien").val("");
+	$("#nr_wydruku,#kwota_zl,#kwota_gr").val("");
 	var operation = $("#captcha-operation").html();
 	var result = eval(operation);
 	var now = new Date();
